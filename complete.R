6def86a10eba55fbe7adda32b7d92768f733c82a
c0rn06 <c0rn06@gmail.com>
2017-03-01 02:27:41 -0600

    Update complete.R

--- complete.R
+++ complete.R
@@ -1,8 +1,8 @@
-complete <- function(directory, pollutant, id = 1:332){
+complete <- function(directory, id = 1:332) {
   
   # create empty master data frame to store all values 
-  master_frame <- data.frame(1,2)
-  colnames(master_frame) <- c("id", "nobs")
+  master_frame <- data.frame()
+
   
   #create a list of all of the file names in the directory
   location_files <- list.files(directory, full.names = TRUE)
@@ -12,17 +12,16 @@ complete <- function(directory, pollutant, id = 1:332){
     #get the location data frame
     location <- read.csv(location_files[i])
     
-    goodrows <- nrow(complete.cases(location))
-    temp_frame <- data.frame(1,2)
-    colnames(temp_frame) <- c("id", "nobs")
-    temp_frame[1,1]<-i
-    temp_frame[1,2]<-goodrows
+    nobs <- sum(complete.cases(location))
     
+    
+    temp_frame <- data.frame(i, nobs)
+    #colnames(temp_frame) <- c("id", "nobs")
     
     #add/bind it to the master frame
     master_frame <- rbind(master_frame, temp_frame)
   }
-  
-  print(master_frame)
+  colnames(master_frame) <- c("id", "nobs")
+  return(master_frame)
   
 }
